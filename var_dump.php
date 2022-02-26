107f94e84dc079ef8f08ce7ad8e5694d96ed86a0
Muhammad Nur Arssy <87927396+muhammadarssy@users.noreply.github.com>
2022-02-26 18:42:15 +0700

    Add files via upload

---
+++ var_dump.php
@@ -0,0 +1,12 @@
+<?php
+// var_dump() untuk menampilkan informasi tentang variable
+
+var_dump(1 + 1);
+echo "<br>";
+var_dump("1 + 1");
+echo "<br>";
+var_dump(1 - "1");
+echo "<br>";
+var_dump("1" - "1");
+
+?>
